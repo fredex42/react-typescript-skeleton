b29ec4914d71454308e2786d666dc505d82f0a87
Andy Gallagher <andy.gallagher@theguardian.com>
2022-02-26 09:35:16 +0000

    adding initial dependency lists from mediabrowser

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var APP_DIR = path.resolve(__dirname, 'app');
+
+var config = {
+    entry: APP_DIR + '/index.tsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [".js",".jsx",".ts",".tsx",".svg",".css"],
+        fallback: {
+            stream: require.resolve("stream-browserify"),
+            util: require.resolve("util/"),
+            crypto: require.resolve("crypto-browserify"),
+            buffer: require.resolve("buffer/")
+        },
+    },
+    optimization: {
+        minimizer: [new TerserPlugin()]
+    },
+    module : {
+        rules : [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "source-map-loader",
+            },
+            {
+                test: /\.m?js$/,
+                resolve: {
+                    fullySpecified: false,
+                },
+                type: "javascript/auto", //see https://github.com/webpack/webpack/issues/11467
+            },
+            {
+                test: /\.[tj]sx?/,
+                include: APP_DIR,
+                loader: "ts-loader",
+            },
+            {
+                test: /\.svg$/,
+                use: ["@svgr/webpack", "url-loader"],
+            },
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+        }),
+    ],
+};
+
+module.exports = config;
